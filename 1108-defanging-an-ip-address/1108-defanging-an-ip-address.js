5275dffdc9254fadee1f22fcfd7215e597fea8ba
Amalshad <amalshad69@gmail.com>
2026-06-10 09:06:30 +0530

    Time: 46 ms (32.02%), Space: 54.1 MB (16.54%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -3,13 +3,5 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    let ip=''
-    for(let char of address){
-        if(char==='.'){
-            ip+='[.]'
-        }else{
-            ip+=char
-        }
-    }
-    return ip
+    return address.split('.').join('[.]')
 };
