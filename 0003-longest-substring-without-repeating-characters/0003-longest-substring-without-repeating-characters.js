07936306634ba23b47750284f7ce946bf5203619
Amalshad <amalshad69@gmail.com>
2026-06-20 00:23:17 +0530

    Time: 5 ms (77.23%), Space: 56.2 MB (87.52%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+
+    let map = new Map();
+
+    let left = 0;
+    let maxLength = 0;
+
+    for(let right = 0; right < s.length; right++) {
+
+        let char = s[right];
+
+        if(map.has(char) && map.get(char) >= left) {
+            left = map.get(char) + 1;
+        }
+
+        map.set(char, right);
+
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+};
