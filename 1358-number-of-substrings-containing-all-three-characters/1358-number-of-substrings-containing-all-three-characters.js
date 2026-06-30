7e7f9514073d0cdabcbeb3ae2e2a28035933f64d
Amalshad <amalshad69@gmail.com>
2026-06-30 21:00:39 +0530

    Time: 21 ms (61.11%), Space: 56.7 MB (58.59%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.js
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.js
@@ -5,25 +5,14 @@
 var numberOfSubstrings = function(s) {
 
     let count = 0;
-
-    let freq = {
-        a: 0,
-        b: 0,
-        c: 0
-    };
-
-
+    let freq = {a: 0,b: 0,c: 0};
     let left = 0;
-
 
     for(let right = 0; right < s.length; right++){
 
         freq[s[right]]++;
 
-
-        while(freq.a > 0 &&freq.b > 0 &&
-            freq.c > 0
-        ){
+        while(freq.a > 0 &&freq.b > 0 &&freq.c > 0){
             count += s.length - right;
 
             freq[s[left]]--;
