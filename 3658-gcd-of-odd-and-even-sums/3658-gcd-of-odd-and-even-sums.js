f6a6d1000bee30ecbf0f6d69d88e4020b3c58806
Amalshad <amalshad69@gmail.com>
2026-07-15 12:05:15 +0530

    Time: 1 ms (51.69%), Space: 55.8 MB (47.19%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.js
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.js
@@ -2,22 +2,6 @@
  * @param {number} n
  * @return {number}
  */
-var gcdOfOddEvenSums = function(n) {
-    let sumOdd=0
-    let sumEven=0
-    let GCD=0
-
-    for(let i=1;i<=n;i++){
-        if(i%2==0) sumEven+=i
-        else sumOdd+=i
-    }
-
-    for(let i=1;i<=sumOdd;i++){
-        if(sumOdd%i==0&&sumEven%i==0){
-            GCD=Math.max(GCD,i)
-        }
-    }
-
+var gcdOfOddEvenSums = function (n) {
     return n
-    
 };
