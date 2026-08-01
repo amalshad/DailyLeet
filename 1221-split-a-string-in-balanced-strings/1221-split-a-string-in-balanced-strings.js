67fc823d1489b1a5aa0077f8aac8693578449cd1
Amalshad <amalshad69@gmail.com>
2026-08-01 09:26:48 +0530

    Time: 0 ms (100%), Space: 54 MB (63.03%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let count =0
+    let result=0
+
+    for(let char of s){
+        if(char==="L") count++;
+        else count--;
+
+        if(count==0) result++
+    }
+
+    return result;
+};
